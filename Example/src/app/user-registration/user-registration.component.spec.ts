d55a3c5c992ed6f41f03bfe0f22644aebfea73c2
ankitmaurya9042 <ankitmaurya9042@gamil.com>
2023-04-25 08:29:10 +0530

    example file showing problem

--- Example/src/app/user-registration/user-registration.component.spec.ts
+++ Example/src/app/user-registration/user-registration.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+
 
 import { UserRegistrationComponent } from './user-registration.component';
 
@@ -21,3 +21,7 @@ describe('UserRegistrationComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+function beforeEach(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
